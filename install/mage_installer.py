dc2abaf88637629b8d21a877d25016d3ceabbb3c
Dan LaManna <dan.lamanna@gmail.com>
2012-05-26 10:42:42 -0400

    Cleaning up install/prompting.

--- install/mage_installer.py
+++ install/mage_installer.py
@@ -4,7 +4,7 @@ import subprocess
 from subprocess import call
 
 from utils import server
-from utils.server import get_server_config
+from utils.server import get_server_config, getpass_validate_regexp
 
 import installer
 import json, getpass
@@ -19,39 +19,27 @@ class mage_installer(installer.installer):
     install_details  = {}
     
     def _modifying_files(self):
-        local("mv %(dest)s/magento/* %(dest)s" % { "dest": self.dest })
-        
+        local("mv %(dest)s/magento/* %(dest)s"           % { "dest": self.dest })
         local("chmod -R o+w %(dest)s/media %(dest)s/var" % { "dest": self.dest })
-
-        local("chmod o+w %(dest)s/app/etc" % { "dest": self.dest })
+        local("chmod o+w %(dest)s/app/etc"               % { "dest": self.dest })
 
     def _prompt_for_details(self):
-        default_host = "localhost"
-
-        self.install_details["db_username"] = "dlamanna"#raw_input("Database Username: ")
-        self.install_details["db_password"] = ""#raw_input("Password: ")
-        self.install_details["db_name"]     = "dlamanna_testinstaller_magento"#raw_input("Database Name?: ")
-        self.install_details["db_host"]     = "localhost"#raw_input("Please enter name: %s" % default_host + chr(8)*4)
-        self.install_details["admin_email"]  = "dan@intellisites.com"
-        self.install_details["admin_username"] = "admin"
-        self.install_details["admin_pass"]  = "letmein2"#raw_input("Admin Password?: ")
-        # @todo - validate password for alphanumeric
-        self.install_details["store_url"]   = "http://dlamanna.testinstaller.dev.com/"#raw_input("Store Url (with trailing slash): ")
-        
-
-        if not self.install_details["db_host"]:
-            self.install_details["db_host"] = default_host
+        self.install_details["admin_email"]    = prompt("Admin Email: ", validate="[^@]+@[^@]+\.[^@]+")
+        self.install_details["admin_username"] = prompt("Admin User: ", default="admin")
+        self.install_details["admin_pass"]     = getpass_validate_regexp("Admin Password: ", "^\w+$")
+        self.install_details["store_url"]      = prompt("Store URL: ").rstrip("/") + "/"
 
     def _install_package(self):
-        local('php -f %(dest)s/install.php -- --license_agreement_accepted "yes" --locale "en_US" --timezone "America/New_York"  --default_currency "USD" --db_host "%(db_host)s" --db_name "%(db_name)s"        --db_user "%(db_user)s"        --db_pass "%(db_pass)s"        --url "%(store_url)s"        --use_rewrites "yes"        --use_secure "no" --use_secure_admin "no"       --secure_base_url ""        --admin_firstname "John"        --admin_lastname "Doe"        --admin_email "%(admin_email)s"        --admin_username "%(admin_user)s"        --admin_password "%(admin_pass)s"' % { "dest": self.dest,
-                                               "db_host":self.install_details["db_host"],
-                                               "db_name":self.install_details["db_name"],
-                                               "db_user":self.install_details["db_username"],
-                                               "db_pass":self.install_details["db_password"],
-                                               "store_url":self.install_details["store_url"],
-                                               "admin_email":self.install_details["admin_email"] ,
-                                               "admin_user":self.install_details["admin_username"],
-                                               "admin_pass":self.install_details["admin_pass"]} )
+        local('php -f %(dest)s/install.php -- --license_agreement_accepted "yes" --locale "en_US" --timezone "America/New_York" --default_currency "USD" --db_host "%(db_host)s" --db_name "%(db_name)s" --db_user "%(db_user)s" --db_pass "%(db_pass)s" --url "%(store_url)s" --use_rewrites "yes" --use_secure "no" --use_secure_admin "no" --secure_base_url "" --admin_firstname "John" --admin_lastname "Doe" --admin_email "%(admin_email)s" --admin_username "%(admin_user)s" --admin_password "%(admin_pass)s"' %
+              { "dest":        self.dest,
+                "db_host":     self.db_details["database_host"],
+                "db_name":     self.db_details["database_name"],
+                "db_user":     self.db_details["database_user"],
+                "db_pass":     self.db_details["database_pass"].replace("\\", ""),
+                "store_url":   self.install_details["store_url"],
+                "admin_email": self.install_details["admin_email"] ,
+                "admin_user":  self.install_details["admin_username"],
+                "admin_pass":  self.install_details["admin_pass"] })
     
     def _clean_files(self):
         return True
