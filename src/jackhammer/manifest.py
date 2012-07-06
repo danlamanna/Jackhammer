53f069e66c48cfd3ea271c10a80ce8af13489dc4
Dan LaManna <dan.lamanna@gmail.com>
2012-07-05 21:58:37 -0400

    Modifying manifest methods to use mongodb based project manifest.

--- src/jackhammer/manifest.py
+++ src/jackhammer/manifest.py
@@ -1,6 +1,22 @@
 import json
 
-def get_project_manifest():
-    manifest = open('/home/dlamanna/app/etc/manifests/projects.json').read()
+import pymongo
+from pymongo import Connection
 
-    return json.loads(manifest if manifest != '' else '{ "projects": {} }')
+conn     = Connection('localhost', 27017)
+db       = conn.jackhammer
+projects = db.projects
+
+def get_project_manifest():
+    project_manifest = []
+    
+    return [ project_manifest.append(project) for project in projects.find() ]
+
+def get_project(args={}):
+    return projects.find_one(args)
+
+def get_count_from_manifest(args={}):
+    return projects.find(args).count()
+
+def project_exists(slug):
+    return bool(get_count_from_manifest({ "slug": slug }))
