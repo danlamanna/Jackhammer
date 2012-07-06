48635f43c51718551af71603cc2165ed591f52d8
Dan LaManna <dan.lamanna@gmail.com>
2012-07-05 22:00:06 -0400

    Renaming method, fixing edge case of replacement being None.

--- src/jackhammer/utils/replacer.py
+++ src/jackhammer/utils/replacer.py
@@ -4,6 +4,9 @@ Replacement for ${string}
 import re
 from fabric.operations import local
 from fabric.api import *
-def burst_replace(string, replacement, text):
-    from fabric.operations import local
-    return local("sed 's/" + re.escape(string) + '/' + re.escape(replacement) + '/g ' + text)
+
+def jh_replace(string, replacement, text):
+    if replacement is None:
+        replacement = ""
+        
+    return text.replace("${%s}" % str(string), replacement)
